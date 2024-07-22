4a573c573374dbafd89c8e5cc000de75e266333b
mdriyasm <101933358+mdriyasm@users.noreply.github.com>
2024-07-22 16:11:54 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// script.js
+
+// Smooth scroll for navigation links
+document.querySelectorAll('.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
